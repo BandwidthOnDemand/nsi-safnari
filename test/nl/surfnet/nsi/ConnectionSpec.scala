9ac5fc95b9ef3f8abfc434edc738e4c212e304ff
Erik Rozendaal <erik@deler.org>
2013-04-17 16:34:44 +0200

    Make tests less dependent on implementation details.

--- test/nl/surfnet/nsi/ConnectionSpec.scala
+++ test/nl/surfnet/nsi/ConnectionSpec.scala
@@ -12,43 +12,50 @@ class ConnectionSpec extends org.specs2.mutable.Specification {
   val Headers = NsiHeaders(UUID.randomUUID, Some(URI.create("http://example.com/")))
   val ConnectionId = "ConnectionId"
   val CorrelationId = newCorrelationId
-  val InitialConnection = NewConnection(ConnectionId).handle(Inbound(Reserve(Headers)))._1.handle(Outbound(ReserveResponse(Headers.copy(replyTo = None), ConnectionId)))
+  val InitialMessages = Seq(Inbound(Reserve(Headers)), Outbound(ReserveResponse(Headers.copy(replyTo = None), ConnectionId)))
+  val InitialConnection = given(InitialMessages: _*)
+
+  def given(messages: Message*): Connection = messages.foldLeft(NewConnection(ConnectionId): Connection) { _.handle(_)._1 }
+  def when(connection: Connection, message: Message): Seq[Message] = connection.handle(message)._2
 
   "A connection" should {
     "send a reserve response when reserve is requested" in {
-      val Seq(response: ReserveResponse) = NewConnection(ConnectionId).handle(Inbound(Reserve(Headers)))._2
+      val connection = given()
+
+      val Seq(response: ReserveResponse) = when(connection, Inbound(Reserve(Headers)))
 
       response.headers must beEqualTo(Headers.copy(replyTo = None))
     }
 
     "send a path computation request when reserve is received" in {
-      val connection = NewConnection(ConnectionId).handle(Inbound(Reserve(Headers)))._1
+      val connection = given(Inbound(Reserve(Headers)))
 
-      val outbound = connection.handle(Outbound(ReserveResponse(Headers, ConnectionId)))._2
+      val outbound = when(connection, Outbound(ReserveResponse(Headers, ConnectionId)))
 
       outbound must haveOneElementLike { case request: PathComputationRequest => ok }
     }
 
     "fail the connection when path computation fails" in {
-      val connection = InitialConnection._1.handle(Outbound(PathComputationRequest(CorrelationId)))._1
+      val connection = given(InitialMessages :+ Outbound(PathComputationRequest(CorrelationId)): _*)
 
-      val outbound = connection.handle(Inbound(PathComputationFailed(CorrelationId)))._2
+      val outbound = when(connection, Inbound(PathComputationFailed(CorrelationId)))
 
       outbound must contain(ReserveFailed(Headers.copy(replyTo = None), ConnectionId))
     }
 
     "mark the reservation state failed" in {
-      val connection = InitialConnection._1.handle(Outbound(ReserveFailed(Headers.copy(replyTo = None), ConnectionId)))._1
+      val connection = given(InitialMessages :+ Outbound(ReserveFailed(Headers.copy(replyTo = None), ConnectionId)): _*)
 
       connection.reservationState must beEqualTo(FailedReservationState)
     }
 
     "confirm the reservation with a single path segment" in {
-      val connection = InitialConnection._1.handle(Inbound(PathComputationConfirmed(CorrelationId)))._1.
-        handle(Outbound(Reserve(Headers)))._1.
-        handle(Inbound(ReserveResponse(Headers, "SegmentConnectionId")))._1
+      val connection = given(InitialMessages ++ Seq(
+        Inbound(PathComputationConfirmed(CorrelationId)),
+        Outbound(Reserve(Headers)),
+        Inbound(ReserveResponse(Headers, "SegmentConnectionId"))): _*)
 
-      val outbound = connection.handle(Inbound(ReserveConfirmed()))._2
+      val outbound = when(connection, Inbound(ReserveConfirmed()))
 
       outbound must contain(ReserveConfirmed())
     }
