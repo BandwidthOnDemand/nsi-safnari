119a9cf377b7e9c9842e1531485f52fb94466759
Erik Rozendaal <erik@deler.org>
2013-04-24 11:59:45 +0200

    Try more isolation to fix Jenkins build.

--- test/nl/surfnet/nsi/ConnectionSpec.scala
+++ test/nl/surfnet/nsi/ConnectionSpec.scala
@@ -16,48 +16,56 @@ import org.specs2.execute.PendingUntilFixed
 import org.specs2.execute.PendingUntilFixed
 import org.ogf.schemas.nsi._2013._04.connection.types.ReserveType
 import org.ogf.schemas.nsi._2013._04.connection.types.ReservationRequestCriteriaType
+import org.specs2.mutable.After
 
 @org.junit.runner.RunWith(classOf[org.specs2.runner.JUnitRunner])
 class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConversions with PendingUntilFixed {
-  implicit val system = ActorSystem("test")
-  implicit val timeout = Timeout(2.seconds)
 
-  isolated
+  trait fixture extends After {
+    implicit val system = ActorSystem("test-" + UUID.randomUUID().toString)
+    implicit val timeout = Timeout(2.seconds)
 
-  val A = new ReservationRequestCriteriaType
-  val B = new ReservationRequestCriteriaType
+    override def after = {
+      system.shutdown
+      system.awaitTermination
+    }
 
-  val Headers = NsiHeaders(UUID.randomUUID, "RequesterNSA", "ProviderNSA", Some(URI.create("http://example.com/")))
-  val ConnectionId = "ConnectionId"
-  val CorrelationId = newCorrelationId
-  val InitialMessages = Seq(Inbound(Reserve(CorrelationId, new ReserveType)))
+    val A = new ReservationRequestCriteriaType
+    val B = new ReservationRequestCriteriaType
 
-  var messages: Seq[Message] = Nil
+    val Headers = NsiHeaders(UUID.randomUUID, "RequesterNSA", "ProviderNSA", Some(URI.create("http://example.com/")))
+    val ConnectionId = "ConnectionId"
+    val CorrelationId = newCorrelationId
+    val InitialMessages = Seq(Inbound(Reserve(CorrelationId, new ReserveType)))
 
-  val connection = TestFSMRef(new ConnectionActor(ConnectionId, Uuid.mockUuidGenerator(1), TestActorRef(new Actor {
-    def receive = { case m: Message => messages = messages :+ m }})))
+    var messages: Seq[Message] = Nil
 
-  def given(messages: Message*): Unit = messages.foreach(m => connection ! m)
+    val connection = TestFSMRef(new ConnectionActor(ConnectionId, Uuid.mockUuidGenerator(1), TestActorRef(new Actor {
+      def receive = { case m: Message => messages = messages :+ m }
+    })))
 
-  def when(message: Message): Message = {
-    messages = Nil
-    Await.result(connection ? message, Duration.Inf)
+    def given(messages: Message*): Unit = messages.foreach(m => connection ! m)
+
+    def when(message: Message): Message = {
+      messages = Nil
+      Await.result(connection ? message, Duration.Inf)
+    }
   }
 
   "A connection" should {
-    "send a reserve response when reserve is requested" in {
+    "send a reserve response when reserve is requested" in new fixture {
       val ack = when(Inbound(Reserve(CorrelationId, new ReserveType)))
 
       ack.asInstanceOf[ReserveResponse].correlationId must beEqualTo(CorrelationId)
     }
 
-    "send a path computation request when reserve is received" in {
+    "send a path computation request when reserve is received" in new fixture {
       when(Inbound(Reserve(CorrelationId, new ReserveType)))
 
       messages must haveOneElementLike { case request: PathComputationRequest => ok }
     }
 
-    "send reserve requests to when path computation confirmed is received" in {
+    "send reserve requests to when path computation confirmed is received" in new fixture {
       given(InitialMessages: _*)
 
       when(Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A, B))))
@@ -65,7 +73,7 @@ class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConvers
       messages must haveSize(2)
     }
 
-    "fail the connection when path computation fails" in {
+    "fail the connection when path computation fails" in new fixture {
       given(InitialMessages: _*)
 
       when(Inbound(PathComputationFailed(new UUID(0, 1))))
@@ -74,7 +82,7 @@ class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConvers
       connection.stateName must beEqualTo(FailedReservationState)
     }
 
-    "confirm the reservation with a single path segment" in {
+    "confirm the reservation with a single path segment" in new fixture {
       given(InitialMessages ++ Seq(
         Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A)))): _*)
 
@@ -85,9 +93,9 @@ class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConvers
       connection.stateName must beEqualTo(HeldReservationState)
     }
 
-    "be in reservation held state when both segments are confirmed" in {
+    "be in reservation held state when both segments are confirmed" in new fixture {
       given(InitialMessages ++ Seq(
-          Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A, B)))): _*)
+        Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A, B)))): _*)
 
       when(Inbound(ReserveConfirmed(new UUID(0, 2), "ConnectionIdA")))
       messages must beEmpty
@@ -98,7 +106,7 @@ class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConvers
       connection.stateName must beEqualTo(HeldReservationState)
     }
 
-    "fail the reservation with a single path segment" in {
+    "fail the reservation with a single path segment" in new fixture {
       given(InitialMessages ++ Seq(
         Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A)))): _*)
 
@@ -108,18 +116,18 @@ class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConvers
       connection.stateName must beEqualTo(FailedReservationState)
     }
 
-    "fail the reservation with two segments and at least one fails" in {
+    "fail the reservation with two segments and at least one fails" in new fixture {
       given(InitialMessages ++ Seq(
         Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A, B))),
         Inbound(ReserveFailed(new UUID(0, 2), ConnectionId))): _*)
 
-     when(Inbound(ReserveConfirmed(new UUID(0, 3), "connectionIdB")))
+      when(Inbound(ReserveConfirmed(new UUID(0, 3), "connectionIdB")))
 
-     messages must contain(ReserveFailed(CorrelationId, ConnectionId))
-     connection.stateName must beEqualTo(FailedReservationState)
+      messages must contain(ReserveFailed(CorrelationId, ConnectionId))
+      connection.stateName must beEqualTo(FailedReservationState)
     }
 
-    "be in committing state when reserve commit is received" in {
+    "be in committing state when reserve commit is received" in new fixture {
       given(InitialMessages ++ Seq(
         Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A))),
         Inbound(ReserveConfirmed(new UUID(0, 2), ConnectionId))): _*)
@@ -130,7 +138,7 @@ class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConvers
       connection.stateName must beEqualTo(CommittingReservationState)
     }
 
-    "be in reserved state when reserve commit confirmed is received" in {
+    "be in reserved state when reserve commit confirmed is received" in new fixture {
       val CommitCorrelationId = newCorrelationId
       given(InitialMessages ++ Seq(
         Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A))),
@@ -143,7 +151,7 @@ class ConnectionSpec extends org.specs2.mutable.Specification with NoTimeConvers
       connection.stateName must beEqualTo(ReservedReservationState)
     }
 
-    "be in aborting state when reserve abort is received" in {
+    "be in aborting state when reserve abort is received" in new fixture {
       given(InitialMessages ++ Seq(
         Inbound(PathComputationConfirmed(new UUID(0, 1), Seq(A))),
         Inbound(ReserveConfirmed(new UUID(0, 2), "ConnectionIdA"))): _*)
