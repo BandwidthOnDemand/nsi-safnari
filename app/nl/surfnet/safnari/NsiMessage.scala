177081570bdbcd9bd10acd34905384239bd69875
Erik Rozendaal <erik@deler.org>
2013-05-13 15:47:48 +0200

    Predefined error messages from draft standard.

--- app/nl/surfnet/safnari/NsiMessage.scala
+++ app/nl/surfnet/safnari/NsiMessage.scala
@@ -16,3 +16,22 @@ trait NsiMessage {
 }
 trait NsiQuery
 trait NsiCommand
+
+final case class NsiError(id: String, description: String, text: String) {
+  override def toString = s"$id: $description: $text"
+}
+object NsiError {
+  val MissingParameter = NsiError("SVC0001", "MISSING_PARAMETER", "Invalid or missing parameter")
+  val UnsupportedOperation = NsiError("SVC0002", "UNSUPPORTED_OPERATION", "Parameter provided contains an unsupported value which MUST be processed")
+  val AlreadyExists = NsiError("SVC0003", "ALREADY_EXISTS", "Schedule already exists for connectionId")
+  val DoesNotExist = NsiError("SVC0004", "DOES_NOT_EXIST", "Schedule does not exist for connectionId")
+  val MissingSecurity = NsiError("SVC0005", "MISSING_SECURITY", "Invalid or missing user credentials")
+  val TopologyResolutionStp = NsiError("SVC0006", "TOPOLOGY_RESOLUTION_STP", "Could not resolve STP in Topology database")
+  val TopologyResolutionStpNsa = NsiError("SVC0007", "TOPOLOGY_RESOLUTION_STP_NSA", "Could not resolve STP to managing NSA")
+  val PathComputationNoPath = NsiError("SVC0008", "PATH_COMPUTATION_NO_PATH", "Path computation failed to resolve route for reservation")
+  val InvalidState = NsiError("SVC0009", "INVALID_STATE", "Connection state machine is in invalid state for received message")
+  val InternalError = NsiError("SVC0010", "INTERNAL_ERROR", "An internal error has caused a message processing failure")
+  val InternalNrmError = NsiError("SVC0011", "INTERNAL_NRM_ERROR", "An internal NRM error has caused a message processing failure")
+  val StpAlreadyInUse = NsiError("SVC0012", "STP_ALREADY_IN_USE", "Specified STP already in use")
+  val BandwidthNotAvailable = NsiError("SVC0012", "BANDWIDTH_NOT_AVAILABLE", "Insufficient bandwidth available for reservation")
+}
