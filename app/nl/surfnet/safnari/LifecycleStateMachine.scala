5d7533d02007752c2c14690fca40b8cadac32d6a
Erik Rozendaal <erik@deler.org>
2013-05-16 10:47:45 +0200

    Updated lifecycle state machine for consistency with reservation state machine. Added stricter message checking.

--- app/nl/surfnet/safnari/LifecycleStateMachine.scala
+++ app/nl/surfnet/safnari/LifecycleStateMachine.scala
@@ -5,36 +5,39 @@ import org.ogf.schemas.nsi._2013._04.connection.types.LifecycleStateEnumType
 import org.ogf.schemas.nsi._2013._04.connection.types.LifecycleStateType
 
 
-case class LifecycleStateMachineData(providers: Map[ConnectionId, ProviderEndPoint], states: Map[ConnectionId, LifecycleStateEnumType], correlationId: Option[CorrelationId] = None) {
+case class LifecycleStateMachineData(children: Map[ConnectionId, ProviderEndPoint], childStates: Map[ConnectionId, LifecycleStateEnumType], correlationId: Option[CorrelationId] = None) {
 
-  def initialize(providers: Map[ConnectionId, ProviderEndPoint]) = {
-    LifecycleStateMachineData(providers, providers.keys.map(_ -> UNKNOWN).toMap)
+  def initialize(children: Map[ConnectionId, ProviderEndPoint]) = {
+    LifecycleStateMachineData(children, children.keys.map(_ -> INITIAL).toMap)
   }
 
   def aggregatedLifecycleStatus: LifecycleStateEnumType = {
-    if (states.values.exists(_ == UNKNOWN)) UNKNOWN
-    else if (states.values.exists(_ == TERMINATING)) TERMINATING
-    else if (states.values.forall(_ == INITIAL)) INITIAL
-    else if (states.values.forall(_ == TERMINATED)) TERMINATED
-    else throw new IllegalStateException(s"cannot determine aggregated status from ${states.values}")
+    if (childStates.values.exists(_ == UNKNOWN)) UNKNOWN
+    else if (childStates.values.forall(_ == INITIAL)) INITIAL
+    else if (childStates.values.exists(_ == TERMINATING)) TERMINATING
+    else if (childStates.values.forall(_ == TERMINATED)) TERMINATED
+    else throw new IllegalStateException(s"cannot determine aggregated status from ${childStates.values}")
   }
 
-  def updateState(connectionId: ConnectionId, state: LifecycleStateEnumType) = {
-    copy(states = states + (connectionId -> state))
-  }
+  def updateChild(connectionId: ConnectionId, state: LifecycleStateEnumType) =
+    copy(childStates = childStates.updated(connectionId, state))
+
+  def childHasState(connectionId: ConnectionId, state: LifecycleStateEnumType) =
+    childStates.getOrElse(connectionId, UNKNOWN) == state
 }
 
 class LifecycleStateMachine(connectionId: ConnectionId, newCorrelationId: () => CorrelationId, outbound: Message => Unit) extends FiniteStateMachine(INITIAL, new LifecycleStateMachineData(Map.empty, Map.empty)) {
 
   when(INITIAL) {
-    case Event(downstreamConnections: Map[_, _], data) =>
-      stay using data.initialize(downstreamConnections.map(p => p._1.asInstanceOf[ConnectionId] -> p._2.asInstanceOf[ProviderEndPoint]))
-    case Event(FromRequester(message: Terminate), data) => goto(TERMINATING) using(data.copy(correlationId = Some(message.correlationId))) replying GenericAck(message.correlationId)
+    case Event(children: Map[_, _], data) =>
+      stay using data.initialize(children.map(p => p._1.asInstanceOf[ConnectionId] -> p._2.asInstanceOf[ProviderEndPoint]))
+    case Event(FromRequester(message: Terminate), data) =>
+      goto(TERMINATING) using(data.copy(correlationId = Some(message.correlationId))) replying GenericAck(message.correlationId)
   }
 
   when(TERMINATING) {
-    case Event(FromProvider(message: TerminateConfirmed), data) =>
-      val newData = data.updateState(message.connectionId, TERMINATED)
+    case Event(FromProvider(message: TerminateConfirmed), data) if data.childHasState(message.connectionId, INITIAL)=>
+      val newData = data.updateChild(message.connectionId, TERMINATED)
       goto(newData.aggregatedLifecycleStatus) using(newData) replying GenericAck(message.correlationId)
   }
 
@@ -42,7 +45,7 @@ class LifecycleStateMachine(connectionId: ConnectionId, newCorrelationId: () => 
 
   onTransition {
     case INITIAL -> TERMINATING =>
-      stateData.providers.foreach {
+      stateData.children.foreach {
         case (connectionId, provider) =>
           outbound(ToProvider(Terminate(newCorrelationId(), connectionId), provider))
       }
