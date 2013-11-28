49af02dadd5f407b09e4857b9862f7ed91832494
Erik Rozendaal <erik@deler.org>
2013-11-28 15:45:47 +0100

    Refactored IdempotentProvider for clarity.

--- app/nl/surfnet/safnari/IdempotentProvider.scala
+++ app/nl/surfnet/safnari/IdempotentProvider.scala
@@ -2,42 +2,55 @@ package nl.surfnet.safnari
 
 import org.ogf.schemas.nsi._2013._07.framework.types.ServiceExceptionType
 
+private case class RequesterCommandStatus(command: FromRequester, outstandingCommandsOrReply: Either[Map[CorrelationId, OutboundMessage], ToRequester]) {
+  def replySent(reply: ToRequester) = {
+    assert(command.correlationId == reply.correlationId, s"reply $reply correlationId did not match command $command correlationId")
+    copy(outstandingCommandsOrReply = Right(reply))
+  }
+
+  def commandsSent(commands: Seq[OutboundMessage]) = copy(outstandingCommandsOrReply = outstandingCommandsOrReply.left.map(_ ++ commands.map(m => m.correlationId -> m)))
+
+  def commandReplyReceived(outstandingCommand: InboundMessage) = copy(outstandingCommandsOrReply = outstandingCommandsOrReply match {
+    case Left(outstandingCommands) => Left(outstandingCommands - outstandingCommand.correlationId)
+    case reply @ Right(_)          => reply
+  })
+}
+
 class IdempotentProvider(providerNsa: String, wrapped: InboundMessage => Option[Seq[OutboundMessage]]) extends (InboundMessage => Either[ServiceExceptionType, (Boolean, Seq[OutboundMessage])]) {
-  private var fromRequester = Map.empty[CorrelationId, (FromRequester, Option[ToRequester])]
-  private var fromRequesterByDownstreamCorrelationId = Map.empty[CorrelationId, FromRequester]
-  private var downstreamRequestsByFromRequesterCorrelationId = Map.empty[CorrelationId, Map[CorrelationId, OutboundMessage]].withDefaultValue(Map.empty)
+  private var requesterCommands = Map.empty[CorrelationId, RequesterCommandStatus]
+  private var outstandingToRequesterCommands = Map.empty[CorrelationId, CorrelationId]
 
   override def apply(message: InboundMessage) = message match {
     case inbound @ FromRequester(NsiProviderMessage(_, _: NsiProviderCommand)) =>
-      fromRequester.get(inbound.correlationId) match {
+      requesterCommands.get(inbound.correlationId) match {
         case None =>
           val result = wrapped(inbound)
           result.foreach(recordOutput(inbound, _))
           result.map((false, _)).toRight(messageNotApplicable(inbound))
-        case Some((original, result)) =>
+        case Some(RequesterCommandStatus(original, outstandingCommandsOrReply)) =>
           if (!sameMessage(inbound.message, original.message)) {
             Left(NsiError.PayloadError.toServiceException(providerNsa).withText(s"duplicate request with existing correlation id ${inbound.correlationId} does not match the original"))
           } else {
-            Right((true, result.fold {
-              val downstreamMessages = downstreamRequestsByFromRequesterCorrelationId(inbound.correlationId)
-              downstreamMessages.values.to[List]
-            } { reply =>
-              List(reply)
+            Right((true, outstandingCommandsOrReply match {
+              case Left(outstandingCommands) =>
+                outstandingCommands.values.to[Seq]
+              case Right(reply) =>
+                List(reply)
             }))
           }
       }
     case _: FromProvider | _: FromPce =>
-      val originalRequest = fromRequesterByDownstreamCorrelationId.get(message.correlationId)
-      val result = originalRequest match {
+      val result = outstandingToRequesterCommands.get(message.correlationId).flatMap(requesterCommands.get) match {
         case None =>
           wrapped(message)
-        case Some(originalRequest) =>
-          val result = wrapped(message)
-          result.foreach { output =>
-            recordOutput(originalRequest, output)
-            downstreamRequestsByFromRequesterCorrelationId += originalRequest.correlationId -> (downstreamRequestsByFromRequesterCorrelationId(originalRequest.correlationId) - message.correlationId)
+        case Some(RequesterCommandStatus(originalRequest, _)) =>
+          outstandingToRequesterCommands -= message.correlationId
+          val output = wrapped(message)
+          output.foreach { messages =>
+            recordOutput(originalRequest, messages)
+            requesterCommands += originalRequest.correlationId -> requesterCommands(originalRequest.correlationId).commandReplyReceived(message)
           }
-          result
+          output
       }
       result.map((false, _)).toRight(messageNotApplicable(message))
     case _: MessageDeliveryFailure | _: PassedEndTime | _: AckFromProvider | _: FromRequester =>
@@ -45,23 +58,26 @@ class IdempotentProvider(providerNsa: String, wrapped: InboundMessage => Option[
       result.map((false, _)).toRight(messageNotApplicable(message))
   }
 
-  private def recordOutput(inbound: FromRequester, output: Seq[OutboundMessage]): Unit = {
-    val toRequester = output.collectFirst {
-      case outbound @ ToRequester(NsiRequesterMessage(_, _: NsiCommandReply)) =>
-        assert(inbound.correlationId == outbound.correlationId, s"reply correlationId ${outbound.correlationId} did not match request correlationId ${inbound.correlationId}")
-        outbound
+  private def recordOutput(requesterCommand: FromRequester, output: Seq[OutboundMessage]): Unit = {
+    val requesterReply = output.collectFirst {
+      case reply @ ToRequester(NsiRequesterMessage(_, _: NsiCommandReply)) => reply
     }
-    var outboundMessagesByCorrelationId = output.foldLeft(Map.empty[CorrelationId, OutboundMessage]) { (acc, message) => acc + (message.correlationId -> message) }
-    output.foreach { downstream =>
-      downstream match {
-        case _: ToPce | _: ToProvider =>
-          fromRequesterByDownstreamCorrelationId += downstream.correlationId -> inbound
-          downstreamRequestsByFromRequesterCorrelationId += inbound.correlationId -> (downstreamRequestsByFromRequesterCorrelationId(inbound.correlationId) + (downstream.correlationId -> downstream))
-        case _: ToRequester =>
-        // Ignore
-      }
+
+    val newOutstandingCommands = output.collect {
+      case message: ToPce      => message
+      case message: ToProvider => message
     }
-    fromRequester += inbound.correlationId -> (inbound -> toRequester)
+    outstandingToRequesterCommands ++= newOutstandingCommands.map { _.correlationId -> requesterCommand.correlationId }
+
+    val currentStatus = requesterCommands.getOrElse(requesterCommand.correlationId, RequesterCommandStatus(requesterCommand, Left(Map.empty)))
+
+    val updatedStatus = requesterReply match {
+      case None =>
+        currentStatus.commandsSent(newOutstandingCommands)
+      case Some(reply) =>
+        currentStatus.replySent(reply)
+    }
+    requesterCommands = requesterCommands.updated(requesterCommand.correlationId, updatedStatus)
   }
 
   private def sameMessage(a: NsiProviderMessage[NsiProviderOperation], b: NsiProviderMessage[NsiProviderOperation]): Boolean = {
