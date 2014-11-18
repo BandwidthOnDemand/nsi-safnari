e4c6153c2b176b8e612aad802bbebedabaf8840d
Erik Rozendaal <erik@deler.org>
2014-11-18 16:45:04 +0100

    Avoid global configuration state.
    This causes tests to fail based on execution order. Using defs instead
    of lazy vals may become a performance problem.

--- app/controllers/Configuration.scala
+++ app/controllers/Configuration.scala
@@ -10,30 +10,30 @@ import nl.surfnet.safnari._
 
 object Configuration {
   val StartTime = DateTime.now
-  lazy val NsaId = getStringOrFail("safnari.nsa.id")
-  lazy val NsaName = getStringOrFail("safnari.nsa.name")
-  lazy val AdminContactGiven = getStringOrFail("safnari.adminContact.given")
-  lazy val AdminContactSurname = getStringOrFail("safnari.adminContact.surname")
-  lazy val AdminContact = s"$AdminContactGiven $AdminContactSurname"
-  lazy val AdminContactProdid = getStringOrFail("safnari.adminContact.prodid")
-  lazy val Use2WayTLS = current.configuration.getBoolean("nsi.twoway.tls").getOrElse(sys.error("nsi.twoway.tls option is not set"))
-  lazy val VersionString = s"${BuildInfo.version} (${BuildInfo.gitHeadCommitSha})"
-  lazy val Longitude = getStringOrFail("safnari.location.longitude")
-  lazy val Latitude = getStringOrFail("safnari.location.latitude")
-  lazy val AsyncReplyTimeout = readFiniteDuration("safnari.async.reply.timeout")
-  lazy val NetworkId = current.configuration.getString("safnari.network.id")
-  lazy val NetworkUrl = current.configuration.getString("safnari.network.url")
-  lazy val DdsUrl = current.configuration.getString("safnari.dds.url")
-  lazy val PceAlgorithm: PathComputationAlgorithm = current.configuration.getString("pce.algorithm").flatMap(PathComputationAlgorithm.parse).getOrElse(sys.error("pce.algorithm option is not set or invalid"))
-  lazy val PceEndpoint = getStringOrFail("pce.endpoint")
+  def NsaId = getStringOrFail("safnari.nsa.id")
+  def NsaName = getStringOrFail("safnari.nsa.name")
+  def AdminContactGiven = getStringOrFail("safnari.adminContact.given")
+  def AdminContactSurname = getStringOrFail("safnari.adminContact.surname")
+  def AdminContact = s"$AdminContactGiven $AdminContactSurname"
+  def AdminContactProdid = getStringOrFail("safnari.adminContact.prodid")
+  def Use2WayTLS = current.configuration.getBoolean("nsi.twoway.tls").getOrElse(sys.error("nsi.twoway.tls option is not set"))
+  def VersionString = s"${BuildInfo.version} (${BuildInfo.gitHeadCommitSha})"
+  def Longitude = getStringOrFail("safnari.location.longitude")
+  def Latitude = getStringOrFail("safnari.location.latitude")
+  def AsyncReplyTimeout = readFiniteDuration("safnari.async.reply.timeout")
+  def NetworkId = current.configuration.getString("safnari.network.id")
+  def NetworkUrl = current.configuration.getString("safnari.network.url")
+  def DdsUrl = current.configuration.getString("safnari.dds.url")
+  def PceAlgorithm: PathComputationAlgorithm = current.configuration.getString("pce.algorithm").flatMap(PathComputationAlgorithm.parse).getOrElse(sys.error("pce.algorithm option is not set or invalid"))
+  def PceEndpoint = getStringOrFail("pce.endpoint")
 
-  lazy val PeersWith: Seq[PeerEntity] = {
+  def PeersWith: Seq[PeerEntity] = {
     val configOption = current.configuration.getConfigList("safnari.peersWith")
     configOption.toSeq.flatMap(_.asScala.map( peer => PeerEntity(peer.getString("id"), peer.getString("dn")) ))
   }
 
   // Web page footer information for main.scala.html.
-  lazy val WebParams: Map[String,String] = Map("favicon" -> getStringOrFail("web.favicon"), "footer" -> getStringOrFail("web.footer"), "contactURL" -> getStringOrFail("web.contactURL"), "contactText" -> getStringOrFail("web.contactText"))
+  def WebParams: Map[String,String] = Map("favicon" -> getStringOrFail("web.favicon"), "footer" -> getStringOrFail("web.footer"), "contactURL" -> getStringOrFail("web.contactURL"), "contactText" -> getStringOrFail("web.contactText"))
 
   def ConnectionExpirationTime = readFiniteDuration("safnari.connection.expiration.time")
 
