7eab874945a62fa9521fbd9df4aab883b6421b6d
Erik Rozendaal <erik@deler.org>
2016-01-27 16:31:39 +0100

    Copyright 2016

--- app/nl/surfnet/safnari/ConnectionData.scala
+++ app/nl/surfnet/safnari/ConnectionData.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2013, 2014, 2015 SURFnet BV
+ * Copyright (c) 2012, 2013, 2014, 2015, 2016 SURFnet BV
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
